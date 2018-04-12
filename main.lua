2d4bb994dfd5d77eb646fb531b16323e33726169
Sebastian N <sebastian.navas@ocsbstudent.ca>
2018-04-12 12:59:47 -0400

    Negatives and zero
    Now it can calculate negative numbers and zero

--- main.lua
+++ main.lua
@@ -10,25 +10,25 @@
 -- Visual elements
 display.setDefault('background', 0.1, 0.1, 0.3)
 local myTitle = display.newText('ICS2O MULTIPLICATION', display.contentCenterX, 200, native.SystemFont, 100)
-local mySign = display.newText('X', display.contentCenterX - 100, 600, native.SystemFont, 100)
-local myEqual = display.newText('=', display.contentCenterX + 500, 600, native.SystemFont, 100)
+local mySign = display.newText('X', display.contentCenterX - 200, 600, native.SystemFont, 100)
+local myEqual = display.newText('=', display.contentCenterX + 400, 600, native.SystemFont, 100)
 
 
 -- Multiples boxes
-local mutlipleOneBox = native.newTextField(display.contentCenterX - 400, 600, 350, 150)
+local mutlipleOneBox = native.newTextField(display.contentCenterX - 500, 600, 350, 150)
 mutlipleOneBox.id = 'the multiple one'
 
-local mutlipleTwoBox = native.newTextField(display.contentCenterX + 200, 600, 350, 150)
+local mutlipleTwoBox = native.newTextField(display.contentCenterX + 100, 600, 350, 150)
 mutlipleTwoBox.id = 'the multiple two'
 
 -- Initiator button
 local myButton = display.newImageRect('./Assets/Sprites/enterButton.png', 350, 250)
-myButton.x = display.contentCenterX - 80
+myButton.x = display.contentCenterX - 180
 myButton.y = display.contentCenterY + 200
 myButton.id = 'the initiator'
 
 -- the result
-local myResult = display.newText('', display.contentCenterX + 700, 600, native.SystemFont, 150)
+local myResult = display.newText('', display.contentCenterX + 700, 600, native.SystemFont, 130)
 
 --function
 local function addingUp(event)
@@ -38,18 +38,25 @@ local function addingUp(event)
 	local theAddition = 0
 
 	-- If statement
+		
 	if multipleTwo > 0 then
 		repeat
 			theAddition = theAddition + multipleOne
 			multipleTwo = multipleTwo - 1 
 		until multipleTwo == 0
 		myResult.text = theAddition
+	elseif multipleTwo < 0 then
+		repeat
+			theAddition = theAddition + (-multipleOne)
+			multipleTwo = multipleTwo + 1 
+		until multipleTwo == 0
+		myResult.text = theAddition
+	elseif multipleTwo == 0 then
+		myResult.text = theAddition
+	else 
+		myResult.text = 'Your input is invalid!'
 	end
 end
-
-
-
-
 
 -- Event Listener
 myButton:addEventListener('touch', addingUp)
